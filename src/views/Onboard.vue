52be28ce877de0ef87ee466ad6e87ffaa254e7a4
Huynie <huychaudesign@gmail.com>
2021-10-30 20:25:30 -0700

    reverted to 'progress bar correct math'

--- src/views/Onboard.vue
+++ src/views/Onboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home w-full">
-    Onboard
+    Onboard 2
     <div class="bg-gray-100 w-full h-screen">banner</div>
     <div class="bg-gray-300 w-full h-screen">Find out your personality type</div>
     <div class="bg-gray-100 w-full h-screen">Track and compare your results to determine health status</div>
