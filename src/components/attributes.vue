02fa56f18e5e832ecded96e27e707df257afa24d
Huynie <huyniemac@Huynies-MacBook-Pro.local>
2022-01-05 11:51:56 -0800

    radar chart started

---
+++ src/components/attributes.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Attribute
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Attribute",
+    components: {
+        
+    },
+    data() {
+      return {
+        
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
