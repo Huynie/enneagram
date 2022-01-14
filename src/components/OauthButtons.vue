df37d1860d8215605bddbfce0b3d9fe2b637c613
Huynie <huychaudesign@gmail.com>
2022-01-13 23:46:40 -0800

    to do: link providers to same email during login

--- src/components/OauthButtons.vue
+++ src/components/OauthButtons.vue
@@ -107,56 +107,56 @@ export default {
         this.error = true;
         this.errorMsg = error;
         console.log(error.code, provider.credentialFromError(error));
-          // An error happened.
-        if (error.code === 'auth/account-exists-with-different-credential') {
-          // Step 2.
-          // User's email already exists.
-          // The pending Google credential.
-          var pendingCred = error.credential;
-          // The provider account's email address.
-          var email = error.email;
-          // Get sign-in methods for this email.
-          fetchSignInMethodsForEmail(email).then(function(methods) {
-            // Step 3.
-            // If the user has several sign-in methods,
-            // the first method in the list will be the "recommended" method to use.
-            if (methods[0] === 'password') {
-              // Asks the user their password.
-              // In real scenario, you should handle this asynchronously.
-              var password = promptUserForPassword(); // TODO: implement promptUserForPassword.
-              signInWithEmailAndPassword(email, password).then(function(result) {
-                // Step 4a.
-                return linkWithCredential(pendingCred);
-              }).then(function() {
-                // Google account successfully linked to the existing Firebase user.
-                goToApp();
-              });
-              return;
-            }
-            // All the other cases are external providers.
-            // Construct provider object for that provider.
-            // TODO: implement getProviderForProviderId.
-            var provider = getProviderForProviderId(methods[0]);
-            // At this point, you should let the user know that they already have an account
-            // but with a different provider, and let them validate the fact they want to
-            // sign in with this provider.
-            // Sign in to provider. Note: browsers usually block popup triggered asynchronously,
-            // so in real scenario you should ask the user to click on a "continue" button
-            // that will trigger the signInWithPopup.
-            signInWithPopup(provider).then(function(result) {
-              // Remember that the user may have signed in with an account that has a different email
-              // address than the first one. This can happen as Firebase doesn't control the provider's
-              // sign in flow and the user is free to login using whichever account they own.
-              // Step 4b.
-              // Link to Google credential.
-              // As we have access to the pending credential, we can directly call the link method.
-              linkAndRetrieveDataWithCredential(pendingCred).then(function(usercred) {
-                // Google account successfully linked to the existing Firebase user.
-                goToApp();
-              });
-            });
-          });
-        }
+          // TO DO: Link providers to same email
+        // if (error.code === 'auth/account-exists-with-different-credential') {
+        //   // Step 2.
+        //   // User's email already exists.
+        //   // The pending Google credential.
+        //   var pendingCred = error.credential;
+        //   // The provider account's email address.
+        //   var email = error.email;
+        //   // Get sign-in methods for this email.
+        //   fetchSignInMethodsForEmail(email).then(function(methods) {
+        //     // Step 3.
+        //     // If the user has several sign-in methods,
+        //     // the first method in the list will be the "recommended" method to use.
+        //     if (methods[0] === 'password') {
+        //       // Asks the user their password.
+        //       // In real scenario, you should handle this asynchronously.
+        //       var password = promptUserForPassword(); // TODO: implement promptUserForPassword.
+        //       signInWithEmailAndPassword(email, password).then(function(result) {
+        //         // Step 4a.
+        //         return linkWithCredential(pendingCred);
+        //       }).then(function() {
+        //         // Google account successfully linked to the existing Firebase user.
+        //         goToApp();
+        //       });
+        //       return;
+        //     }
+        //     // All the other cases are external providers.
+        //     // Construct provider object for that provider.
+        //     // TODO: implement getProviderForProviderId.
+        //     var provider = getProviderForProviderId(methods[0]);
+        //     // At this point, you should let the user know that they already have an account
+        //     // but with a different provider, and let them validate the fact they want to
+        //     // sign in with this provider.
+        //     // Sign in to provider. Note: browsers usually block popup triggered asynchronously,
+        //     // so in real scenario you should ask the user to click on a "continue" button
+        //     // that will trigger the signInWithPopup.
+        //     signInWithPopup(provider).then(function(result) {
+        //       // Remember that the user may have signed in with an account that has a different email
+        //       // address than the first one. This can happen as Firebase doesn't control the provider's
+        //       // sign in flow and the user is free to login using whichever account they own.
+        //       // Step 4b.
+        //       // Link to Google credential.
+        //       // As we have access to the pending credential, we can directly call the link method.
+        //       linkAndRetrieveDataWithCredential(pendingCred).then(function(usercred) {
+        //         // Google account successfully linked to the existing Firebase user.
+        //         goToApp();
+        //       });
+        //     });
+        //   });
+        // }
 
       }
     }
