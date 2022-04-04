12fe603016018f93233f7a46a5db7b4884756ab0
Huy Chau <huychaudesign@gmail.com>
2022-04-04 19:17:14 +0000

    Test questions restyled and tally/next logic completed

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,8 +1,16 @@
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-const Button = ({title, onPress, backgroundColor, color}) => {
+const Button = ({title, onPress, backgroundColor, color, disabled}) => {
   return (
-    <TouchableOpacity onPress={onPress} style={{...styles.buttonContainer, backgroundColor: backgroundColor ?? '#89E5CF'}}>
+    <TouchableOpacity 
+      onPress={onPress}
+      style={{
+        ...styles.buttonContainer,
+        backgroundColor: backgroundColor ?? '#89E5CF'
+      }}
+      disabled={disabled}
+      activeOpacity={disabled ? 0.5 : 1}
+    >
       <Text style={{...styles.buttonText, color: color ?? 'white'}}>{title}</Text>
     </TouchableOpacity>
   )
--- src/screens/TestScreen.js
+++ src/screens/TestScreen.js
@@ -1,8 +1,9 @@
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, Pressable } from "react-native";
 import * as React from 'react';
 import Graphic from '../component/graphic';
 import Button from '../component/Button';
 import questions from '../questions.json';
+import { set } from "react-native-reanimated";
 
 export default function TestScreen() {
   const [counter, setCounter]= React.useState(0);
@@ -10,63 +11,63 @@ export default function TestScreen() {
   const [question2, setQuestion2]= React.useState(questions[counter].choice2);
   const [choice, setChoice]= React.useState(null);
   const [tally, setTally] = React.useState({
-    A: 0,
-    B: 0,
-    C: 0,
-    D: 0,
-    E: 0,
-    F: 0,
-    G: 0,
-    H: 0,
-    I: 0,
+    D:0,
+    F:0,
+    C:0,
+    E:0,
+    H:0,
+    B:0,
+    I:0,
+    G:0,
+    A:0,
   });
-  const choice1 = () => {
-    //button focus goes here
-    const answer1 = questions[counter].answer1;
-    setChoice(answer1);
-    // clear focus from choice button 2
-    // add focus to choice button 1
+  const [focused, setFocused] = React.useState(null);
+  const questionSelect = (choiceNum) => {
+    const answer = choiceNum === 1 ? questions[counter].answer1 : questions[counter].answer2;
+    setFocused(choiceNum === 1 ? 'button1' : 'button2');
+    setChoice(answer);
     // enable "next" button
   };
   const next = () => {
-    if(choice !== '') {
-      tally[choice] ++;
+    if(choice) {
+      setTally({...tally, [choice]: + 1});
       // add tally to local storage
-      counter ++
+      setCounter(counter + 1);
       // save counter as progress in local storage
       // setProgressBarFill(`${(parseInt(counter) + 1) * 0.695}%`);
       setQuestion1(questions[counter].choice1)
       setQuestion2(questions[counter].choice2)
-      // clear all focus from buttons
+      setFocused(null);
       // showNext = counter === 143 ? false : true
       // showGetType = counter === 143 ? true : false
     }else{
-      confirm('Please choose an answer.')
+      console.log('Please choose an answer.')
       return
     }
-
-    choice = '';
-    disabledBtn = true
   }
 
   return (
     <View>
       <Graphic />
       <View style={styles.screenContainer}>
-        <TouchableOpacity
-          style={styles.questionContainer}
-          onPress={choice1}
+        <Pressable
+          style={{ ...styles.questionContainer, ...focused === 'button1' ? styles.focused : null}}
+          onPress={() => questionSelect(1)}
         >
           <Text style={styles.questions}>{question1}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.questionContainer}>
+        </Pressable>
+        <Pressable
+          style={{ ...styles.questionContainer, ...focused === 'button2' ? styles.focused : null}}
+          onPress={() => questionSelect(2)}
+        >
           <Text style={styles.questions}>{question2}</Text>
-        </TouchableOpacity>
+        </Pressable>
         <Button 
           title="next"
           backgroundColor="#89E5CF"
           color="white"
           onPress={next}
+          // disabled={true}
         />
       </View>
     </View>
@@ -81,15 +82,26 @@ const styles = StyleSheet.create({
   questionContainer: {
     backgroundColor: "#89E5CF",
     borderRadius: 15,
-    minHeight: 100,
-    minWidth: 200,
+    minHeight: 150,
+    minWidth: 300,
     marginVertical: 20,
     justifyContent: 'center',
     alignItems: 'center',
-    alignSelf: 'center',
+    // width: 300,
+    // height: 150,
+    // padding: 5
   },
   questions: {
     color: 'white',
     fontSize: 20,
+    // marginHorizontal: 'auto',
+    maxWidth: 250,
+    // borderWidth: 2,
+  },
+  focused: {
+    borderColor: '#EC4899',
+    borderWidth: 3,
+    borderStyle: 'solid',
+    backgroundColor: '#67D2D2'
   }
 })
