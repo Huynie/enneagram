34442139ff40a02c1c671d0def0bf1fe9b0b50ba
Huy Chau <huychaudesign@gmail.com>
2022-04-17 03:42:25 +0000

    forgot pw page error message

--- src/screens/ForgotPassword.js
+++ src/screens/ForgotPassword.js
@@ -3,29 +3,50 @@ import { View, Text, TextInput, StyleSheet } from 'react-native';
 import Graphic from '../component/graphic';
 import Button from '../component/Button';
 import { Input } from 'react-native-elements';
+import { sendPasswordResetEmail } from '@firebase/auth';
+import { auth, db } from "../firebase/config";
 
 const ForgotPassword = () => {
   const [email, setEmail] = React.useState('');
+  const [error, setError] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState(false);
 
   const resetPassword = () => {
     if(email !== '') {
-      console.log('PW Reset!');
+      sendPasswordResetEmail(auth, email)
+        .then(() => {
+          console.log(email);
+          setErrorMessage("If your account exists you will receive an email");
+          setError(true);
+        }).catch(err => {
+          console.log(err.message, email);
+          setError(true);
+          setErrorMessage(err.message);
+        })
       return
     };
-    console.log('please enter your email!');
+    setError(true);
+    setErrorMessage('Please enter your email to reset your password.');
   }
 
   return (
     <View>
       <Graphic/>
       <View style={styles.forgotPwContainer}>
-        <Text style={{fontSize: 16, color: 'gray', marginBottom: 20}}>Please enter your email to reset your password.</Text>
+        <Text style={{color: 'gray', marginBottom: 20}}>Please enter your email to reset your password.</Text>
         <TextInput 
           placeholder="Email"
           style={styles.input}
           value={email}
           onChangeText={(value) => setEmail(value)}
         />
+        <View>
+          {error ? 
+          <Text style={{fontSize: 14, color: 'red', marginBottom: 20}}>{errorMessage}</Text>
+          :
+          <></>
+          }
+        </View>
         <Button
           title="Reset"
           onPress={() => resetPassword(email)}
