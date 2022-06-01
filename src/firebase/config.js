54658192aa249e8b944017b691b517f0e9effc78
Huy Chau <huychaudesign@gmail.com>
2022-06-01 17:15:50 +0000

    updated dependencies

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+
+import {initializeApp} from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore/lite';
+
+export const firebaseApp = initializeApp({
+  apiKey: "AIzaSyCULs8RNL-aOMXhpBUrX7Mn7fyIov9h6Tc",
+  authDomain: "en-gram.firebaseapp.com",
+  projectId: "en-gram",
+  storageBucket: "en-gram.appspot.com",
+  messagingSenderId: "1096010449978",
+  appId: "1:1096010449978:web:ce2c4b7cf9f955d3746103",
+  measurementId: "G-K9KVYT5P2M"
+  
+});
+
+const db = getFirestore(firebaseApp);
+export const auth = getAuth(firebaseApp);
+
+export default db;
